1c052fcb8bd08d13fea8eee1888ae7ff722eca82
aleksandrvolynec <amvolinec@gmail.com>
2023-08-02 22:55:04 +0300

    ATM class create

--- src/ATM/ATM.java
+++ src/ATM/ATM.java
@@ -2,6 +2,9 @@ package ATM;
 
 public class ATM {
     public static void main(String[] args) {
+        Account userOne = new Account(1234,6789,2300);
+        Account userTwo = new Account(5678,1234,4600);
 
+        Bankomat bankomat  = new Bankomat(1, 2, 3, 4, 5);
     }
 }
