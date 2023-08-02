49148ebdc153701473384e03a526af8af20dd27c
aleksandrvolynec <amvolinec@gmail.com>
2023-08-02 22:14:04 +0300

    Account complited

--- src/ATM/Account.java
+++ src/ATM/Account.java
@@ -5,4 +5,43 @@ public class Account {
     private int pinNumber;
     private double totalBalance = 0;
     private double availableBalance = 0;
+
+    public Account(int customerNumber,int pinNumber,double totalBalance){
+        this.customerNumber = customerNumber;
+        this.pinNumber = pinNumber;
+        this.totalBalance = totalBalance;
+        this.availableBalance = totalBalance;
+    }
+
+    public int getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(int customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public int getPinNumber() {
+        return pinNumber;
+    }
+
+    public void setPinNumber(int pinNumber) {
+        this.pinNumber = pinNumber;
+    }
+
+    public double getTotalBalance() {
+        return totalBalance;
+    }
+
+    public void setTotalBalance(double totalBalance) {
+        this.totalBalance = totalBalance;
+    }
+
+    public double getAvailableBalance() {
+        return availableBalance;
+    }
+
+    public void setAvailableBalance(double availableBalance) {
+        this.availableBalance = availableBalance;
+    }
 }
