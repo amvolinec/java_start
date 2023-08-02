f54619b35cc12f6462fde231f7128c808d5ae0b6
irynasluchova <irynasluchova@gmail.com>
2023-08-02 23:01:49 +0300

    ATM updated

--- src/ATM/ATM.java
+++ src/ATM/ATM.java
@@ -2,9 +2,12 @@ package ATM;
 
 public class ATM {
     public static void main(String[] args) {
-        Account userOne = new Account(1234,6789,2300);
-        Account userTwo = new Account(5678,1234,4600);
+        Account[] users = new Account[2];
+        users[0] = new Account(1234,6789,2300);
+        users[1] = new Account(5678,1234,4600);
 
-        Bankomat bankomat  = new Bankomat(1, 2, 3, 4, 5);
+        Bankomat bankomat  = new Bankomat(10, 20, 10, 25, 12);
+
+
     }
 }
