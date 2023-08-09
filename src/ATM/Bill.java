fec4441fc1b6897295c90bd3e6039e88d8adbff2
irynasluchova <irynasluchova@gmail.com>
2023-08-09 08:35:18 +0300

    Bill class added

---
+++ src/ATM/Bill.java
@@ -0,0 +1,22 @@
+package ATM;
+
+public class Bill {
+    private int nominal;
+    private int count;
+
+    public int getNominal() {
+        return nominal;
+    }
+
+    public void setNominal(int nominal) {
+        this.nominal = nominal;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
