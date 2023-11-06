a67c56d963dc2ce00c8729433c91e15366ed3cfb
aleksandrvolynec <amvolinec@gmail.com>
2023-11-06 20:33:01 +0200

    Updated

--- src/NovemberSixExam/SecondTask.java
+++ src/NovemberSixExam/SecondTask.java
@@ -10,33 +10,32 @@ public class SecondTask {
 
     private static final int today = LocalDate.now().getDayOfMonth();
 
+    private static final int month = LocalDate.now().getMonthValue();
+
     public static int arraySize() {
-        int size;
-        return Math.round((8511 - today) / 5 + 5);
+        return Math.round((8511 - today) / 5);
     }
 
     private static int array[] = new int[arraySize()];
 
-    public static void printEachNumberAfterSubstracting() {
+    public static void initArray() {
         int a = 0;
         for (int i = 0; i < array.length; i++) {
             array[i] = today + a;
-            a += 20;
-
-            System.out.println(array[i] - today + 20);
+            a += 5;
         }
     }
 
     private static void print() {
         for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i] - today);
+            System.out.println(array[i] - month + 20);
         }
     }
 
     private static void printEachNumberWhile() {
         int i = 0;
         while (i < array.length) {
-            System.out.println(array[i] - today);
+            System.out.println(array[i] - month + 20);
             i++;
         }
     }
@@ -44,7 +43,7 @@ public class SecondTask {
     private static void printEachNumberDoWhile() {
         int i = 0;
         do {
-            System.out.println(array[i] - today);
+            System.out.println(array[i] - month + 20);
             i++;
         } while (i < array.length);
     }
@@ -52,12 +51,12 @@ public class SecondTask {
     private static void printForEach() {
         for (int i : array
         ) {
-            System.out.println(i - today);
+            System.out.println(i - month + 20);
         }
     }
 
     public static void main(String[] args) {
-        printEachNumberAfterSubstracting();
+        initArray();
         print();
         printEachNumberWhile();
         printEachNumberDoWhile();
